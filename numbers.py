d41071c1a959df8e767acd35f0aadc2dba7b478e
Jay Bhakhar <jaybhakhar@gmail.com>
2022-01-29 03:50:03 +0500

    find pi & e to the nth digit

--- numbers.py
+++ numbers.py
@@ -29,4 +29,20 @@ def credit_card_validator():
         print("String is not allow")
 
 
-credit_card_validator()
+from mpmath import mp
+
+
+def find_pi_to_the_nth_digit():
+    mp.dps = int(input("Enter a number :- ")) + 1
+    if mp.dps < 52:
+        print(mp.pi)
+    else:
+        print("Maximum number is 50")
+
+
+def find_e_to_the_nth_digit():
+    mp.dps = int(input("Enter a number :- ")) + 1
+    if mp.dps < 52:
+        print(mp.e)
+    else:
+        print("Maximum number is 50")
