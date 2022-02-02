7fb92b3f2b0b68b2d9a3f0c7908b396f024686e1
Jay Bhakhar <jaybhakhar@gmail.com>
2022-02-03 02:54:58 +0500

    collatz_conjecture

--- unit_test.py
+++ unit_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+from Classic_Algorithms.collatz_conjecture import collatz_conjecture
 from Numbers.credit_card_validator import is_credit_card_valid
 from Numbers.unit_converter import convert_unit
 
@@ -14,6 +15,13 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(convert_unit('fahrenheit', 'celsius', 50), '50 °F is equal to 10.0008 °C')
         self.assertEqual(convert_unit('kg', 'pound', 71), '71 Kg is equal to 156.52802 Pound')
         self.assertEqual(convert_unit('pound', 'kg', 200), '200 Pound is equal to 90.7184 Kg')
+
+    def test_collatz_conjecture(self):
+        self.assertEqual(collatz_conjecture(31), 1)
+        self.assertEqual(collatz_conjecture(696), 1)
+        self.assertEqual(collatz_conjecture(3111), 1)
+        self.assertEqual(collatz_conjecture(91152), 1)
+        self.assertEqual(collatz_conjecture(999851), 1)
 
 
 if __name__ == '__main__':
