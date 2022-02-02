5e3e379583f1166f820e4ee5b5eb91756e1959f0
Jay Bhakhar <jaybhakhar@gmail.com>
2022-02-03 04:40:28 +0500

    unit_converter changes

--- Numbers/unit_converter.py
+++ Numbers/unit_converter.py
@@ -1,55 +1,20 @@
-# 11 c -> f, 12 f -> c
-# 21EUR, 22UAH, 23USD ----> RUB
-# 31kg -> pound, 32pound -> kg
-
-def convert_unit(frm: str, to: str, value: int) -> str:
+def convert_unit(frm: str, to: str, value: float) -> float:
     frm, to = frm.lower(), to.lower()
-    proper_combinations = [
-        ['celsius', 'fahrenheit'],
-        ['kg', 'pound']
-    ]
 
-    is_valid = False
-    for proper_comb in proper_combinations:
-        if frm and to in proper_comb and frm != to:
-            is_valid = True
-            break
+    if frm == 'celsius' and to == 'fahrenheit':
+        return round((value * 1.8) + 32, 2)
 
-    if not is_valid:
-        return "invalid combination"
+    if frm == 'fahrenheit' and to == 'celsius':
+        return round((value - 32) * 0.5556, 2)
 
-    if frm == proper_combinations[0][0] and to == proper_combinations[0][1]:
-        return f"{value} °C is equal to {celsius_to_fahrenheit(value)} °F"
+    if frm == 'kg' and to == 'pound':
+        return round(value * 2.20462, 3)
 
-    if frm == proper_combinations[0][1] and to == proper_combinations[0][0]:
-        return f"{value} °F is equal to {fahrenheit_to_celsius(value)} °C"
+    if frm == 'pound' and to == 'kg':
+        return round(value * 0.453592, 3)
 
-    if frm == proper_combinations[1][0] and to == proper_combinations[1][1]:
-        return f"{value} Kg is equal to {kg_to_pound(value)} Pound"
-
-    if frm == proper_combinations[1][1] and to == proper_combinations[1][0]:
-        return f"{value} Pound is equal to {pound_to_kg(value)} Kg"
+    raise AttributeError("Unknown units")
 
 
-def celsius_to_fahrenheit(val):
-    return (val * 1.8) + 32
-
-
-def fahrenheit_to_celsius(val):
-    return (val - 32) * 0.5556
-
-
-def kg_to_pound(val):
-    return val*2.20462
-
-
-def pound_to_kg(val):
-    return val*0.453592
-
-
-print(convert_unit('celsius', 'fahrenheit', 12))
-print(convert_unit('fahrenheit', 'celsius', 50))
-print(convert_unit('kg', 'pound', 71))
-print(convert_unit('pound', 'kg', 200))
-
-
+def conversion_result(frm: str, to: str, value: float) -> str:
+    return f"{value} {frm} is equal to {convert_unit(frm, to, value)} {to}"
--- unit_test.py
+++ unit_test.py
@@ -1,19 +1,13 @@
 import unittest
-
-from Numbers.credit_card_validator import is_credit_card_valid
 from Numbers.unit_converter import convert_unit
 
 
 class MyTestCase(unittest.TestCase):
-    # def test_credit_card(self):
-    #     message = "Not valid credit number"
-    #     self.assertEqual(is_credit_card_valid("2******444***961"), True, message)
-
     def test_unit_converter(self):
-        self.assertEqual(convert_unit('celsius', 'fahrenheit', 12), '12 °C is equal to 53.6 °F')
-        self.assertEqual(convert_unit('fahrenheit', 'celsius', 50), '50 °F is equal to 10.0008 °C')
-        self.assertEqual(convert_unit('kg', 'pound', 71), '71 Kg is equal to 156.52802 Pound')
-        self.assertEqual(convert_unit('pound', 'kg', 200), '200 Pound is equal to 90.7184 Kg')
+        self.assertEqual(convert_unit('celsius', 'fahrenheit', 12), 53.6)
+        self.assertEqual(convert_unit('fahrenheit', 'celsius', 50), 10.00)
+        self.assertEqual(convert_unit('kg', 'pound', 170), 374.785)
+        self.assertEqual(convert_unit('pound', 'kg', 200), 90.718)
 
 
 if __name__ == '__main__':
